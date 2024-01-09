ff612ab8ae21323b6add9652d87363da39cb5a68
KevinDazaR <61398857+KevinDazaR@users.noreply.github.com>
2024-01-09 15:50:57 -0500

    Add files via upload
    Almost done, left the change of languages

--- js/app.js
+++ js/app.js
@@ -1,70 +1,76 @@
-var tema = document.getElementById("tema-select");
-var body = document.getElementById("body");
-
-let localTema = localStorage.getItem("temas");
-
-body.classList.remove("theme-light");
-body.classList.remove("theme-dark");
-body.classList.remove("theme-american");
-body.classList.add(localTema);
-
-
 function cambiarTema(){
 
+    let tema = document.getElementById("tema-select").value;
+    let body = document.getElementById("body");
+    let localTema = localStorage.getItem("temas");
+
+    body.classList.remove("theme-light");
+    body.classList.remove("theme-dark");
+    body.classList.remove("theme-american");
+
+    body.classList.add(localTema);
     console.log(tema);
-    if(tema.value == "light"){ //light
+
+    if(tema == "light"){ //light
         body.classList.add("theme-light");
         body.classList.remove("theme-dark");
         body.classList.remove("theme-american");
-        
 
     }
 
-    else if(tema.value  == "dark"){ //dark
+    else if(tema  == "dark"){ //dark
         body.classList.add("theme-dark")
         body.classList.remove("theme-light");
         body.classList.remove("theme-american");
     }
 
-    else if(tema.value  == "usa"){ //american
+    else if(tema == "american"){ //american
         body.classList.add("theme-american");
         body.classList.remove("theme-dark");
         body.classList.remove("theme-light");
     }
 
-    localStorage.setItem("temas", tema.value);
+    localStorage.setItem("temas", tema);
+    console.log(localStorage);
     
-   
-
-    //     /*Limpiando los campos o inputs*/
-    // document.getElementById("nombretxt").value = "";
-    // document.getElementById("apellidotxt").value = "";
 }
 
 console.log("hola");
 
 
+try{
+    const localTema = localStorage.getItem("temas");
 
 
-// try{
-//     const localTema = localStorage.getItem("temas");
+    if(localTema == "light"){ //light
+        body.classList.add("theme-light");
+        body.classList.remove("theme-dark");
+        body.classList.remove("theme-american");
+    }
 
+    else if(localTema == "dark"){ //dark
+        body.classList.add("theme-dark")
+        body.classList.remove("theme-light");
+        body.classList.remove("theme-american");
+    }
 
-//     if(localTema == "light"){ //light
-//         body.classList.add("theme-light");
-//         body.classList.remove("theme-dark");
-//         body.classList.remove("theme-american");
-//     }
+    else if(localTema == "usa"){ //american
+        body.classList.add("theme-american");
+        body.classList.remove("theme-dark");
+        body.classList.remove("theme-light");
+    }
+}catch (error){}
 
-//     else if(localTema == "dark"){ //dark
-//         body.classList.add("theme-dark")
-//         body.classList.remove("theme-light");
-//         body.classList.remove("theme-american");
-//     }
+function cambiarLanguage(){
 
-//     else if(localTema == "usa"){ //american
-//         body.classList.add("theme-american");
-//         body.classList.remove("theme-dark");
-//         body.classList.remove("theme-light");
-//     }
-// }catch (error){}
+    let langaugesValue = document.getElementById("languages").value;
+
+    console.log(langaugesValue);
+
+    if(langaugesValue == "español"){
+        location.href = "index.html"
+    }
+    else{
+        location.href = "./html/index_english.html"
+    }
+}
